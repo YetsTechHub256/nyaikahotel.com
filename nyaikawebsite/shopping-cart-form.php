15a7acfaad6151a81e094ec1e26fe93982671c92
YetsTechHub256 <136362089+YetsTechHub256@users.noreply.github.com>
2023-12-10 09:34:23 +0300

    Add files via upload

---
+++ nyaikawebsite/shopping-cart-form.php
@@ -0,0 +1,36 @@
+<form action="pesapal-iframe.php" method="post">
+	<table>
+		<tr>
+			<td>Amount:</td>
+			<td><input type="text" name="amount" value="5000" />
+			(ugsh)
+			</td>
+		</tr>
+		<tr>
+			<td>Type:</td>
+			<td><input type="text" name="type" value="MERCHANT" readonly="readonly" />
+			(leave as default - MERCHANT)
+			</td>
+		</tr>
+		<tr>
+			<td>Description:</td>
+			<td><input type="text" name="description" value="Order Description" /></td>
+		</tr>
+		
+		<tr>
+			<td>First Name:</td>
+			<td><input type="text" name="first_name" value="owen" /></td>
+		</tr>
+		<tr>
+			<td> Last Name:</td>
+			<td><input type="text" name="last_name" value="moris" /></td>
+		</tr>
+		<tr>
+			<td> Email Address:</td>
+			<td><input type="text" name="email" value="kmorisowen@gmail.com" /></td>
+		</tr>
+		<tr>
+			<td colspan="2"><input type="submit" value="Donate" /></td>
+		</tr>
+	</table>
+</form>
