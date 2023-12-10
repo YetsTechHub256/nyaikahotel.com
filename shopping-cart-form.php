419fb7b71550bb1a4742bb2586f90a308e6ac446
YetsTechHub256 <136362089+YetsTechHub256@users.noreply.github.com>
2023-12-10 11:11:38 +0300

    Update shopping-cart-form.php

--- shopping-cart-form.php
+++ shopping-cart-form.php
@@ -1,9 +1,61 @@
+<style>
+{
+    font-family: 'Arial', sans-serif;
+    background-color: #f4f4f4;
+}
+
+form {
+    max-width: 400px;
+    margin: 20px auto;
+    background-color: #fff;
+    padding: 20px;
+    border-radius: 8px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+table {
+    width: 100%;
+}
+
+table td {
+    padding: 10px;
+}
+
+input[type="text"] {
+    width: 100%;
+    padding: 8px;
+    margin-bottom: 10px;
+    box-sizing: border-box;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+}
+
+input[type="submit"] {
+    background-color: #4caf50;
+    color: white;
+    padding: 10px 15px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    font-size: 16px;
+}
+
+input[type="submit"]:hover {
+    background-color: #45a049;
+}
+
+@media (max-width: 600px) {
+    form {
+        max-width: 100%;
+    }
+}
+</style>
 <form action="pesapal-iframe.php" method="post">
 	<table>
 		<tr>
 			<td>Amount:</td>
-			<td><input type="text" name="amount" value="5000" />
-			(ugsh)
+			<td><input type="text" name="amount" value="1000" />
+			(UGX)
 			</td>
 		</tr>
 		<tr>
@@ -19,18 +71,18 @@
 		
 		<tr>
 			<td>First Name:</td>
-			<td><input type="text" name="first_name" value="owen" /></td>
+			<td><input type="text" name="first_name" value="Mumbere" /></td>
 		</tr>
 		<tr>
 			<td> Last Name:</td>
-			<td><input type="text" name="last_name" value="moris" /></td>
+			<td><input type="text" name="last_name" value="Deric" /></td>
 		</tr>
 		<tr>
 			<td> Email Address:</td>
-			<td><input type="text" name="email" value="kmorisowen@gmail.com" /></td>
+			<td><input type="text" name="email" value="dericyetsjr@gmail.com" /></td>
 		</tr>
 		<tr>
-			<td colspan="2"><input type="submit" value="Donate" /></td>
+			<td colspan="2"><input type="submit" value="BOOK NOW" /></td>
 		</tr>
 	</table>
 </form>
